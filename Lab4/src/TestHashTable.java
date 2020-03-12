b4fa0ba38ceb528dc68cd9c1bc7c235cdbe8e295
velvid <muntakim.ca@gmail.com>
2020-03-12 00:59:18 -0400

    updated test class

--- Lab4/src/TestHashTable.java
+++ Lab4/src/TestHashTable.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class TestClass {
+public class TestHashTable {
     public static void main(String[] args) {
         
         Random rand = new Random();
@@ -9,9 +9,9 @@ public class TestClass {
         double averageProbesTheoretical = 0;
         int runs = 100;
         int insertions = 100000;
-        double lambdaList[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99};
+        double lambdaList[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95};
         
-        System.out.println("\n<LINEAR PROBING/>");
+        System.out.println("\n<SUCCESSFUL LINEAR PROBING/>");
         for(double lambda : lambdaList) { 
             for(int i=0; i<runs; i++) {       
                 HashTableLin test = new HashTableLin(insertions, lambda);
@@ -28,9 +28,9 @@ public class TestClass {
                     averageProbesTheoretical,
                     averageProbesExperimental);
         }
-        System.out.println("<LINEAR PROBING/>\n");
+        System.out.println("<SUCCESSFUL LINEAR PROBING/>\n");
         
-        System.out.println("\n<QUADRATIC PROBING/>");
+        System.out.println("\n<SUCCESSFUL QUADRATIC PROBING/>");
         for(double lambda : lambdaList) { 
             for(int i=0; i<runs; i++) {       
                 HashTableQuad test = new HashTableQuad(insertions, lambda);
@@ -47,7 +47,37 @@ public class TestClass {
                     averageProbesTheoretical,
                     averageProbesExperimental);
         }
-        System.out.println("</QUADRATIC PROBING>\n");
+        System.out.println("</SUCCESSFUL QUADRATIC PROBING>\n");
+        
+        int numList[] = {1, 5, 7, 11, 11*2, 11*3, 11*4};
+        
+        System.out.println("\n<TEST LINEAR PROBE INSERTION/>\n");
+        HashTableLin test1 = new HashTableLin(10, 0.9);
+        for(int num : numList) {
+            System.out.format("<.insert(%d)>\n", num);
+            test1.insert(num);
+            test1.printKeysAndIndexes();
+            System.out.println("Load:\t" + test1.getLoad());
+            System.out.println("MaxNum:\t" + test1.getMaxNum());
+            System.out.println("Count:\t" + test1.getCount());
+            System.out.println("Length:\t" + test1.getLength());
+            System.out.println();
+        }
+        System.out.println("</TEST LINEAR PROBE INSERTION>\n");
+        
+        System.out.println("\n<TEST QUADRATIC PROBE INSERTION/>\n");
+        HashTableQuad test2 = new HashTableQuad(10, 0.9);
+        for(int num : numList) {
+            System.out.format("<.insert(%d)>\n", num);
+            test2.insert(num);
+            test2.printKeysAndIndexes();
+            System.out.println("Load:\t" + test2.getLoad());
+            System.out.println("MaxNum:\t" + test2.getMaxNum());
+            System.out.println("Count:\t" + test2.getCount());
+            System.out.println("Length:\t" + test2.getLength());
+            System.out.println();
+        }
+        System.out.println("</TEST QUADRATIC PROBE INSERTION>\n");
         
     }
 }
