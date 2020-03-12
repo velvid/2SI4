8e20ff6c75cd13cc57c1e7c6d1d4efbb80f2c1e3
Muntakim <muntakim.ca@gmail.com>
2020-03-12 13:32:35 -0400

    done + commented

--- Lab4/src/HashTableLin.java
+++ Lab4/src/HashTableLin.java
@@ -1,65 +1,74 @@
 
 public class HashTableLin {
     
-    private int[] table;
-    private boolean[] filled;
-    private int length;
-    private int maxNum;
-    private double load;
-    private int count = 0;
-    private int totalProbes = 0;
+    private int[] table;            // to store hash values
+    private boolean[] filled;       // array to determine if table is filled
+    private int length;             // size of the hash table
+    private int maxNum;             // maximum occupied cells allowed
+    private double load;            // max occupied per size allowed
+    private int count = 0;          // count of occupied/filled cells
+    private int totalProbes = 0;    // total probes when inserting
     
+    /* constructor */
     public HashTableLin(int maxNum, double load) {
         this.maxNum = maxNum;
         this.load = load;
-        this.length = findMinPrimeAbove((int)((double)maxNum/load));
-        this.table = new int[length];
-        this.filled = new boolean[length];
+        this.length = findMinPrimeAbove((int)((double)maxNum/load));    // length has to be prime
+        this.table = new int[length];       // all initialized to zero
+        this.filled = new boolean[length];  // all initialized to false
     }
     
+    /* getters for field variables */
     public int getLength()  { return length; }
     public int getMaxNum()  { return maxNum; }
     public double getLoad() { return load; }
     public int getCount()   { return count; }
     public int getTotalProbes() { return totalProbes; }
     
+    /* function to check if ia number is prime */
     public boolean isPrime(int n) {
-        if(n == 1) return false;
-        if(n%2 == 0) return false;
-        for(int i=3; i*i<n; i+=2)
-            if(n%i == 0) return false;
-        return true;
+        if(n == 1) return false;        // 1 is not prime
+        if(n%2 == 0) return false;      // if divisible by 2, not prime
+        for(int i=3; i*i<n; i+=2)       // increment up by 2 and until sqrt(n)
+            if(n%i == 0) return false;  // if divisibly by any number from 3 to n-1, not prime
+        return true;    // if all cases pass, it is prime
     }
     
+    /* finds minimum prime greater than given integer */
     public int findMinPrimeAbove(int n) {
         for(int num=n; true; num++)
             if(isPrime(num)) return num;
     }
     
+    /* inserts a key into the hash table */
     public void insert(int n) {
-        int key = n % length;
-        int oldKey = key;
+        int key = n % length;   // takes mod of key
+        int oldKey = key;       // stores the initial key
         
-        int i=1, probes = 1;
-        while(filled[key]) {
-            if(table[key] == n) { return; }
-            key = (oldKey + i) % length;
+        int i=1, probes = 1;    // counters for probe number and total probes
+        while(filled[key]) {    // if collision...
+            if(table[key] == n) { return; } // if value is in table, do nothing
+            key = (oldKey + i) % length;    // linearly probe to new index
             i++;
             probes++;
         }
         
-        table[key] = n;
-        filled[key] = true;
-        count++;
-        totalProbes += probes;
+        // this block only executes once an empty cell is reached 
+        table[key] = n;         // assign new value to key
+        filled[key] = true;     // cell is now filled
+        count++;                // increment filled counter
+        totalProbes += probes;  // add to total probes
         
-        if(count >= load*length) this.rehash();
+        // check if the table needs to be rehashed
+        if(count > load*length) this.rehash();
     }
     
+    /* removes value from table */
     public void remove(int n) {
         int key = n % length;
         int oldKey = key;
         
+        // probes until value is found
         int i=1;
         while(filled[key]) {
             if(table[key] == n) {
@@ -69,60 +78,68 @@ public class HashTableLin {
             }
             key = (oldKey + i) % length;
             i++;
+            if(i == length) return;     // if for some reason table is full, return
         }
-    }    
+    }
     
+    /* finds if value exists in table */
     public boolean isIn(int n) {
         int key = n % length;
         int oldKey = key;
         
+        // probes until value is found
         int i=1;
         while(filled[key]) {
-            if(table[key] == n) return true;
-            if(i == length) return false;
+            if(table[key] == n) return true;    // if found, return true
+            if(i == length) return false;       // if for some reason table is full
             key = (oldKey + i) % length;
             i++;
         }
-        return false;
+        return false;   // if empty cell found, return false
     }
     
+    /* rehashes 'this' hashtable when load tolerance is exceeded */
     private void rehash() {
-        int newLength = findMinPrimeAbove(2*length);
-        int[] newTable = new int[newLength];
+        int newLength = findMinPrimeAbove(2*length);    // new prime length at least 2 times larger than old
+        int[] newTable = new int[newLength];            // array for new table
         boolean[] newFilled = new boolean[newLength];
         
+        // iterates through old table and hashes each value into new table
         for(int i=0; i<length; i++) {
             if(filled[i]) {
-                int key = table[i] % newLength;
-                int oldKey = key;
-                
-                int j=1;
-                while(newFilled[key]) {
-                    key = (oldKey + j) % newLength;
+                int key = table[i] % newLength;     // hashes to new table
+                int oldKey = key;                   // stores initial key
+                int j=1;                            // offset for linear probe
+                while(newFilled[key]) {         // while there are collisions...
+                    key = (oldKey + j) % newLength; // ...linearly probe
                     j++;
                 }
-                
-                newTable[key] = table[i];
-                newFilled[key] = true;
+                newTable[key] = table[i];   // insert to new table
+                newFilled[key] = true;      // set cell to full
             }
         }
         
+        // update all field variables for 'this'
         length = newLength;
         table = newTable;
-        filled = newFilled;
-        maxNum = (int)((double)(load)*(double)(newLength));
+        filled = newFilled; 
+        double temp = ((double)(load)*(double)(newLength));
+        maxNum = (double)(int)temp == temp ? (int)temp : (int)(temp);
     }
     
+    /* iterates through all elements in table and prints values */
     public void printKeys() {
         for(int i=0; i<length; i++)
             if(filled[i])
                 System.out.println(table[i]);
     }
     
+    /* iterates through all elements in table and prints indexes and values */
     public void printKeysAndIndexes() {
+        System.out.println("Index\tKey");
         for(int i=0; i<length; i++)
             if(filled[i]) System.out.println(i + "\t" + table[i]);
-            else System.out.println(i + "\tnull");
+            else System.out.println(i + "\t");  // if empty
     }
     
 }
--- Lab4/src/TestHashTable.java
+++ Lab4/src/TestHashTable.java
@@ -15,7 +15,7 @@ public class TestHashTable {
         for(double lambda : lambdaList) { 
             for(int i=0; i<runs; i++) {       
                 HashTableLin test = new HashTableLin(insertions, lambda);
-                int upper = 999999999, lower = 1;
+                int upper = 2147483646, lower = 1;
                 for(int j=0; j<insertions; j++)
                     test.insert(rand.nextInt(upper-lower+1)+lower);
                 averageProbesExperimental += 
@@ -34,7 +34,7 @@ public class TestHashTable {
         for(double lambda : lambdaList) { 
             for(int i=0; i<runs; i++) {       
                 HashTableQuad test = new HashTableQuad(insertions, lambda);
-                int upper = 999999999, lower = 1;
+                int upper = 2147483646, lower = 1;
                 for(int j=0; j<insertions; j++)
                     test.insert(rand.nextInt(upper-lower+1)+lower);
                 averageProbesExperimental += 
@@ -51,33 +51,64 @@ public class TestHashTable {
         
         int numList[] = {1, 5, 7, 11, 11*2, 11*3, 11*4};
         
-        System.out.println("\n<TEST LINEAR PROBE INSERTION/>\n");
+        System.out.println("\n<TEST LINEAR PROBE INSERTION/>");
         HashTableLin test1 = new HashTableLin(10, 0.9);
         for(int num : numList) {
             System.out.format("<.insert(%d)>\n", num);
             test1.insert(num);
             test1.printKeysAndIndexes();
+            System.out.format("Current Load: %1.2f\t",
+                    (double)test1.getCount()/(double)test1.getLength());
             System.out.println("Load:\t" + test1.getLoad());
             System.out.println("MaxNum:\t" + test1.getMaxNum());
             System.out.println("Count:\t" + test1.getCount());
             System.out.println("Length:\t" + test1.getLength());
-            System.out.println();
+            if(num != numList[numList.length-1]) System.out.println();
         }
         System.out.println("</TEST LINEAR PROBE INSERTION>\n");
         
-        System.out.println("\n<TEST QUADRATIC PROBE INSERTION/>\n");
+        System.out.println("\n<TEST QUADRATIC PROBE INSERTION/>");
         HashTableQuad test2 = new HashTableQuad(10, 0.9);
         for(int num : numList) {
             System.out.format("<.insert(%d)>\n", num);
             test2.insert(num);
             test2.printKeysAndIndexes();
+            System.out.format("Current Load:% 1.2f\t",
+                    (double)test2.getCount()/(double)test2.getLength());
             System.out.println("Load:\t" + test2.getLoad());
             System.out.println("MaxNum:\t" + test2.getMaxNum());
             System.out.println("Count:\t" + test2.getCount());
             System.out.println("Length:\t" + test2.getLength());
-            System.out.println();
+            if(num != numList[numList.length-1]) System.out.println();
         }
         System.out.println("</TEST QUADRATIC PROBE INSERTION>\n");
         
+        System.out.println("\n<TEST REHASHING/>");
+        HashTableLin test3 = new HashTableLin(5, 0.5);
+        for(int num=1; num<=10; num++) {
+            System.out.format("<.insert(%d)>\n", num);
+            test3.insert(num*num);
+            test3.printKeysAndIndexes();
+            System.out.format("Current Load:% 1.2f\t",
+                    (double)test3.getCount()/(double)test3.getLength());
+            System.out.println("Load:\t" + test3.getLoad());
+            System.out.println("MaxNum:\t" + test3.getMaxNum());
+            System.out.println("Count:\t" + test3.getCount());
+            System.out.println("Length:\t" + test3.getLength());
+            if(num != 10) System.out.println();
+        }
+        System.out.println("</TEST REHASHING>\n");
+        
+        System.out.println("\n<TEST IS IN/>");
+        HashTableLin test4 = new HashTableLin(11, 0.9);
+        for(int num=1; num<=10; num++) test4.insert(num);
+        test4.printKeysAndIndexes();
+        System.out.println();
+        
+        int isInList[] = {1, 3, 6, 10, 11, 13, 100};
+        for(int num : isInList) {
+            System.out.format("isIn(%d):\t%b\n", num, test4.isIn(num));
+        }        
+        System.out.println("</TEST IS IN>\n");
     }
 }
